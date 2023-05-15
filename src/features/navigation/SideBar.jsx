2c2c27b93afece1949e9a10a176f0816d094f8fa
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-05-15 13:12:24 +0100

    Create side bar layout

---
+++ src/features/navigation/SideBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ProfileImage from "src/assets/profile-image.svg";
+import NavItem from "./NavItem";
+
+const SideBar = () => {
+  return (
+    <div className="h-full md:w-1/5 lg:w-1/4 bg-white hidden md:block">
+      <div className="user-data flex gap-5 items-center">
+        <div className="user-image w-20 h-20 rounded-full overflow-hidden">
+          <img src={ProfileImage} alt="profile-img" />
+        </div>
+        <div className="personal-info">
+          <h3 className="user-name font-bold text-xl">Sven Pietsch</h3>
+          <p className="company">Innoloft GmbH</p>
+        </div>
+      </div>
+      <nav className="flex flex-col gap-10 mt-10">
+        <NavItem icon={"home-icon.svg"} text={"home"} />
+        <NavItem icon={"product-icon.svg"} text={"product"} />
+      </nav>
+    </div>
+  );
+};
+
+export default SideBar;
