ce8ac762d80205fb3d741406b45568b2a035a019
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-05-15 10:50:18 +0100

    create config Reducers

--- src/features/config/configSlice.js
+++ src/features/config/configSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axiosBase from "../../services/axiosBase";
 
 const initialState = {
@@ -13,3 +13,23 @@ export const fetchAppConfiguration = createAsyncThunk(
     return axiosBase.get(`/configuration/${APP_ID}/`).then((res) => res.data);
   }
 );
+
+const configSlice = createSlice({
+  name: "config",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchAppConfiguration.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchAppConfiguration.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchAppConfiguration.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default configSlice.reducer;
