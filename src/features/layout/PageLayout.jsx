2d4a49210a0a17df5a6ec1c6e6dd165f1e5c5467
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-05-15 12:15:27 +0100

    Render header to page

--- src/features/layout/PageLayout.jsx
+++ src/features/layout/PageLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "features/header/Header";
+import { useSelector } from "react-redux";
 
 const PageLayout = () => {
+  const { data: appConfig } = useSelector((state) => state.appConfig);
   return (
     <>
-      <Header />
+      <Header mainColor={appConfig.mainColor} />
       <div>Page Navigation Goes Here</div>
       <main className="page-content">
         <Outlet />
