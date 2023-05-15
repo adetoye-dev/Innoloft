f2d6acf3df600944d01a8f75356f2fa4aeb21076
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-05-15 21:31:12 +0100

    Add edit reducers to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import configReducer from "config/configSlice";
 import productReducer from "product/productSlice";
+import categoryReducer from "edit/categories/categorySlice";
+import modelsReducer from "edit/businessModels/modelsSlice";
+import trlReducer from "edit/trl/trlSlice";
 
 const store = configureStore({
   reducer: {
     appConfig: configReducer,
     product: productReducer,
+    categories: categoryReducer,
+    businessModels: modelsReducer,
+    trl: trlReducer,
   },
 });
 
