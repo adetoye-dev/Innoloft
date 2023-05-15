fd941efbf733c68f42a052e39f2e1de6c7d12740
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-05-15 21:11:37 +0100

    Create category actions and reducer

---
+++ src/features/edit/categories/categorySlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchProduct } from "product/productSlice";
+
+const initialState = {
+  data: [],
+};
+
+const categorySlice = createSlice({
+  name: "trl",
+  initialState,
+  reducers: {
+    addCategories: (state, action) => {
+      state.data = [...state.data, { id: "nanoid", name: action.payload }];
+    },
+    removeCategories: (state, action) => {
+      state.data = state.data.filter(
+        (category) => category.id != action.payload
+      );
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchProduct.fulfilled, (state, action) => {
+      state.data = action.payload.categories;
+    });
+  },
+});
+
+export default categorySlice.reducer;
+export const { addCategories, removeCategories } = categorySlice.actions;
