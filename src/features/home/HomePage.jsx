b7e19defa08507d55b559c23a2a160fd7a34e0f6
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-05-16 09:53:31 +0100

    Crete home page view

---
+++ src/features/home/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <iframe
+        src="https://scribehow.com/embed/How_to_View_and_Edit_Products_on_Innoloft_Platform__YNsRHi-tR1GloaAr4WTo8g"
+        width="100%"
+        height="640"
+        allowfullscreen
+        frameborder="0"
+      ></iframe>
+    </>
+  );
+};
+
+export default HomePage;
